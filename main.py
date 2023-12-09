3d052a5226ce48cfc717532c7cae45bd5d6668af
mohankumar <smohankumar018@gmail.com>
2023-12-09 08:46:39 +0530

    final commit

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
 import cv2
 import pandas as pd
 from ultralytics import YOLO
-from tracker import *
-import cvzone
+import math
+class Tracker:
+    def __init__(self):
+        # Store the center positions of the objects
+        self.center_points = {}
+        # Keep the count of the IDs
+        # each time a new object id detected, the count will increase by one
+        self.id_count = 0
+
+
+    def update(self, objects_rect):
+        # Objects boxes and ids
+        objects_bbs_ids = []
+
+        # Get center point of new object
+        for rect in objects_rect:
+            x, y, w, h = rect
+            cx = (x + x + w) // 2
+            cy = (y + y + h) // 2
+
+            # Find out if that object was detected already
+            same_object_detected = False
+            for id, pt in self.center_points.items():
+                dist = math.hypot(cx - pt[0], cy - pt[1])
+
+                if dist < 35:
+                    self.center_points[id] = (cx, cy)
+#                    print(self.center_points)
+                    objects_bbs_ids.append([x, y, w, h, id])
+                    same_object_detected = True
+                    break
+
+            # New object is detected we assign the ID to that object
+            if same_object_detected is False:
+                self.center_points[self.id_count] = (cx, cy)
+                objects_bbs_ids.append([x, y, w, h, self.id_count])
+                self.id_count += 1
+
+        # Clean the dictionary by center points to remove IDS not used anymore
+        new_center_points = {}
+        for obj_bb_id in objects_bbs_ids:
+            _, _, _, _, object_id = obj_bb_id
+            center = self.center_points[object_id]
+            new_center_points[object_id] = center
+
+        # Update dictionary with IDs not used removed
+        self.center_points = new_center_points.copy()
+        return objects_bbs_ids
 
 model = YOLO('yolov8s.pt')
 
@@ -10,31 +56,32 @@ def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
         point = [x, y]
         print(point)
-
-cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
-cap = cv2.VideoCapture(0)  # Use the default camera (change the index if needed)
-
-my_file = open("coco.txt", "r")
-data = my_file.read()
-class_list = data.split("\n")
-
-count = 0
-persondown = {}
-tracker = Tracker()
-counter1 = []
-
-personup = {}
-counter2 = []
-cy1 = 194
-cy2 = 220
-offset = 6
+import os
 
 while True:
+    cv2.namedWindow('RGB')
+    cv2.setMouseCallback('RGB', RGB)
+    cap=cv2.VideoCapture(0)
+    print(cap)
+    my_file = open("coco.txt", "r")
+    data = my_file.read()
+    class_list = data.split("\n")
+
+    count = 0
+    persondown = {}
+    tracker = Tracker()
+    counter1 = []
+
+    personup = {}
+    counter2 = []
+    cy1 = 194
+    cy2 = 220
+    offset = 6
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (1020, 500))
+    frame = cv2.resize(frame, (640, 480))
 
     results = model.predict(frame)
+    #print(results)
     a = results[0].boxes.data
     px = pd.DataFrame(a).astype("float")
     list = []
@@ -47,7 +94,7 @@ while True:
         d = int(row[5])
         c = class_list[d]
         if 'person' in c:
-            list.append([x1, y1, x2, y2])
+             list.append([x1, y1, x2, y2])
 
     bbox_id = tracker.update(list)
     for bbox in bbox_id:
@@ -60,8 +107,9 @@ while True:
     cv2.line(frame, (5, 220), (1019, 220), (0, 255, 255), 2)
 
     cv2.imshow("RGB", frame)
+    
     if cv2.waitKey(1) & 0xFF == 27:
-        break
+        break 
 
 cap.release()
 cv2.destroyAllWindows()
